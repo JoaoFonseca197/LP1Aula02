16710cfae5793f59908883d23aaf6ee7c1a799f4
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-03-05 15:59:37 +0000

    Volume e Raio

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,7 +6,25 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declaração das variavéis 
+            string altura,raio;
+            float alturaF, raioF,v,surfA;
+            const float pi  = 3.1415926f;
+            //1. pedir as variáveis ao utilizador
+            Console.WriteLine("Insira uma altura: ");
+            altura = Console.ReadLine();
+            Console.WriteLine("Insira um raio: ");
+            raio = Console.ReadLine();
+            //2.Converter o input para numeros
+            alturaF = float.Parse(altura);
+            raioF = float.Parse(raio);
+            //3.fazer Contas
+            v = pi * raioF * raioF * alturaF;
+            surfA = 2 * pi * raioF * (raioF+alturaF);
+            //4.Mostrar a solução ao utilizador
+            Console.WriteLine($"Volume = {v,8:f2}");
+            Console.WriteLine($"AreaSup = {surfA,8:f2}");
+
         }
     }
 }
